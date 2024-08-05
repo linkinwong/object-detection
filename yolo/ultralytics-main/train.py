91104dc4817f1c02aafd756e450846757537dcb9
王猛 <wangmeng@wangmengdeMacBook-Air.local>
2024-08-05 14:04:05 +0800

    add code

--- yolo/ultralytics-main/train.py
+++ yolo/ultralytics-main/train.py
@@ -1,9 +1,8 @@
 from ultralytics import YOLO
-
 # Load a model
 model = YOLO("yolov8m.yaml")  # build a new model from YAML
-#model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+  # load a pretrained model (recommended for training)
 
 
 # Train the model
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+results = model.train(data="coco8.yaml", epochs=1000, imgsz=640,device=[0, 1,2,3,4,5,6,7])
